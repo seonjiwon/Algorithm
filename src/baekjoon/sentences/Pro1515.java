6c248cc0793989a4e3a114b1f1e17371da1b49fd
seonjiwon <tommys915@gmail.com>
2025-06-26 09:06:04 +0900

    .

--- src/baekjoon/sentences/Pro1515.java
+++ src/baekjoon/sentences/Pro1515.java
@@ -3,4 +3,36 @@ package baekjoon.sentences;
 import java.io.*;
 
 public class Pro1515 {
+
+    private static final int maxLength = 3000;
+
+    private static int Solution(String number){
+
+        int pointer = 0;
+
+        for (int base = 1; base <= maxLength * 10; base++) {
+            String stringBase = String.valueOf(base);
+            for (int i = 0; i < stringBase.length(); i++) {
+                if (stringBase.charAt(i) == number.charAt(pointer)) {
+                    pointer++;
+                }
+                if (pointer == number.length()) {
+                    return base;
+                }
+            }
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String number = br.readLine();
+
+        int result = Solution(number);
+        System.out.println(result);
+
+        br.close();
+     }
 }
