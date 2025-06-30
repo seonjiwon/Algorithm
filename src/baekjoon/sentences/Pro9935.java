8670abcd659ae325ee5cd72bfe246e83a4407b2d
seonjiwon <tommys915@gmail.com>
2025-06-30 15:40:03 +0900

    .

---
+++ src/baekjoon/sentences/Pro9935.java
@@ -0,0 +1,46 @@
+package baekjoon.sentences;
+
+import java.io.*;
+import java.util.*;
+
+public class Pro9935 {
+    private static String Solution(String str, String bomb){
+        StringBuilder sb = new StringBuilder();
+        int bombLength = bomb.length();
+
+
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.charAt(i));
+
+            if (sb.length() >= bombLength) {
+                boolean isBomb = true;
+                for (int j = 0; j < bombLength; j++) {
+                    if (sb.charAt(sb.length() - bombLength + j) != bomb.charAt(j)) {
+                        isBomb = false;
+                    }
+                }
+
+                if (isBomb) {
+                    sb.setLength(sb.length() - bombLength);
+                }
+            }
+
+
+        }
+
+        return sb.length() == 0 ? "FRULA" : sb.toString();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String bomb = br.readLine();
+
+        String solution = Solution(str, bomb);
+        System.out.println(solution);
+
+        br.close();
+     }
+}
