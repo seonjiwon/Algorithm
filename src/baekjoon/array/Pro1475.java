2b5e3c8e207e16853c19a54943bcd455132e6321
seonjiwon <tommys915@gmail.com>
2025-07-21 13:53:41 +0900

    .

---
+++ src/baekjoon/array/Pro1475.java
@@ -0,0 +1,40 @@
+package baekjoon.array;
+
+import java.io.*;
+import java.util.*;
+
+public class Pro1475 {
+
+    private static int Solution(int n){
+        int[] numberArray = new int[10];
+        String numStr = String.valueOf(n);
+        for (char c : numStr.toCharArray()) {
+            numberArray[c - '0']++;
+        }
+
+        int sixAndNine = (int) Math.round((double)(numberArray[6] + numberArray[9]) / 2);
+
+        int maxValue = 0;
+        for (int i = 0; i < numberArray.length; i++) {
+            if (i == 6 || i == 9) {
+                continue;
+            }
+            if (maxValue < numberArray[i]){
+                maxValue = numberArray[i];
+            }
+        }
+
+        return Math.max(sixAndNine, maxValue);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int number = Integer.parseInt(br.readLine());
+        int solution = Solution(number);
+        System.out.println(solution);
+
+        br.close();
+     }
+}
---
+++ src/baekjoon/array/Pro2577.java
@@ -0,0 +1,35 @@
+package baekjoon.array;
+
+import java.io.*;
+import java.util.*;
+
+public class Pro2577 {
+
+    private static int[] Solution(int n){
+        int[] result = new int[10];
+
+        String str = String.valueOf(n);
+        for(char c : str.toCharArray()){
+            result[c - '0']++;
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n1 = Integer.parseInt(br.readLine());
+        int n2 = Integer.parseInt(br.readLine());
+        int n3 = Integer.parseInt(br.readLine());
+
+        int[] solution = Solution(n1 * n2 * n3);
+
+        for (int i : solution) {
+            System.out.println(i);
+        }
+
+        br.close();
+     }
+}
